6fe00ceeb71687eefb03dc60adb1301a54257311
Deaponn <62819066+Deaponn@users.noreply.github.com>
2022-06-02 18:22:28 +0200

    Update account.js
    Account page is now responsive

--- pages/account.js
+++ pages/account.js
@@ -10,17 +10,34 @@ import ActionButton from "../components/ActionButton";
 const fetcher = (url, method, argument) => fetch(url, { method, body: argument }).then((res) => res.json());
 
 const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
     background-color: ${({ theme }) => theme.secondary};
-    padding-top: 30px;
+    padding: 30px 0;
+
+    @media ${device.tablet} {
+        display: block;
+    }
+
+    @media ${device.desktop} {
+        justify-content: space-evenly;
+    }
 `;
 
 const Profile = styled.div`
+    width: 400px;
+    margin: 30px 30px 0 30px;
+
+    @media ${device.desktop} {
+        margin-right: 0;
+    }
+
     @media ${device.tablet} {
         display: flex;
         flex-direction: column;
         flex-wrap: wrap;
         align-items: center;
-        margin: auto;
+        margin: 0 auto 30px;
         width: min(80%, 426px);
     }
 `;
@@ -36,7 +53,7 @@ const Text = styled.div`
 const Details = styled.div`
     display: flex;
     flex-direction: column;
-    width: 40%;
+    width: calc(100% - 120px);
 `;
 
 const MovieText = styled(Text)`
@@ -52,7 +69,10 @@ const Movies = styled.div`
 
 const Movie = styled.div`
     display: flex;
-    border: 3px solid black;
+    align-items: center;
+    border-top: 3px solid ${({ theme }) => theme.accent.third};
+    margin-bottom: 15px;
+    width: 90%;
 
     @media ${device.tablet} {
         width: min(90%, 475px);
@@ -73,17 +93,18 @@ export default function Account({}) {
     if (fetchError) return <ContentWrapper>{JSON.stringify(fetchError)}</ContentWrapper>;
     if (!sub) return <ContentWrapper>access denied</ContentWrapper>;
 
-    console.log(data)
+    console.log(data);
 
     return (
         <ContentWrapper>
             <Wrapper>
                 <Profile>
                     <Image src={profilePic} placeholder="blur" alt="profile picture" />
-                    <Text>Logged in as: {isLoading ? "loading..." : name}</Text>
+                    <Text>Logged in as:</Text>
+                    <Text>{isLoading ? "loading..." : name}</Text>
                 </Profile>
                 <Movies>
-                    <Text>Movies you have rented:</Text>
+                    <Text style={{ marginBottom: "15px" }}>Movies you have rented:</Text>
                     {!data ? (
                         <Text>Loading movie list...</Text>
                     ) : (
@@ -91,7 +112,7 @@ export default function Account({}) {
                             <Movie key={id}>
                                 <Details>
                                     <MovieText>{title}</MovieText>
-                                    <MovieText>{date}</MovieText>
+                                    <MovieText>At: {date}</MovieText>
                                 </Details>
                                 <ActionButton
                                     action={() => {
